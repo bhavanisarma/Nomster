fc0542c94ee17c5edce916592a811f049c46cefe
bhavanisarma <bhavani.sarma@gmail.com>
2019-08-15 01:53:14 +0000

    OK

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,12 @@
 class Place < ApplicationRecord
 	 belongs_to :user
 	 has_many :comments
+
+	 geocoded_by :address
+	 after_validation :geocode
+	 
 	 validates :name, presence: true, length: { maximum: 140, minimum: 3 }
 	 validates :address, presence: true
 	 validates :description, presence: true
-	 geocoded_by :address
-	 after_validation :geocode
+	 
 end
